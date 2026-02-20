010dcd934341f878d0e7feb32e0cacb9b9fce7f6
NisriinaWakhdah <ninaputri3344@gmail.com>
2026-02-20 10:06:57 +0700

    Add newline

--- src/main/java/id/ac/ui/cs/advprog/yomu/service/TextContextService.java
+++ src/main/java/id/ac/ui/cs/advprog/yomu/service/TextContextService.java
@@ -1,8 +1,6 @@
 package id.ac.ui.cs.advprog.yomu.service;
 
 import id.ac.ui.cs.advprog.yomu.entity.TextContext;
-import id.ac.ui.cs.advprog.yomu.repository.TextContextRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,5 +8,6 @@ import java.util.Optional;
 public interface TextContextService {
 
   List<TextContext> findAll();
+
   Optional<TextContext> findById(String id);
 }
