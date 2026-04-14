27df978808520b47fd7d90e35cb58e3a1031274a
NisriinaWakhdah <ninaputri3344@gmail.com>
2026-04-14 23:05:38 +0700

    Fixing getById() method

--- src/main/java/id/ac/ui/cs/advprog/yomu/controller/AdminReadingController.java
+++ src/main/java/id/ac/ui/cs/advprog/yomu/controller/AdminReadingController.java
@@ -42,11 +42,7 @@ public class AdminReadingController {
   // catch previous data for update reading
   @GetMapping("/{id}")
   public ResponseEntity<Reading> getById(@PathVariable String id) {
-    Reading reading = adminService.findAll().stream()
-        .filter(r -> r.getId().equals(id))
-        .findFirst()
-        .orElseThrow(() -> new RuntimeException("Reading id not found!"));
-    return ResponseEntity.ok(reading);
+    return ResponseEntity.ok(adminService.getById(id));
   }
 
 }
