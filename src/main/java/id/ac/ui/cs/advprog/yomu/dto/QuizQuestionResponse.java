3786b06e21b349213815cbcbbcb3a84d1bfc0fae
NisriinaWakhdah <ninaputri3344@gmail.com>
2026-04-17 14:02:28 +0700

    Fixing dto for question

--- src/main/java/id/ac/ui/cs/advprog/yomu/dto/QuizQuestionResponse.java
+++ src/main/java/id/ac/ui/cs/advprog/yomu/dto/QuizQuestionResponse.java
@@ -3,9 +3,13 @@ package id.ac.ui.cs.advprog.yomu.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @Builder
 public class QuizQuestionResponse {
   private String id;
   private String text;
+  private List<String> options;
+  private String questionType;
 }
