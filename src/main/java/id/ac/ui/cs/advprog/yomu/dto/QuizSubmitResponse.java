0c66c431d050fae5df8b12e466f68f40501a3fc7
NisriinaWakhdah <ninaputri3344@gmail.com>
2026-04-17 15:18:18 +0700

    Add admin quiz controller

--- src/main/java/id/ac/ui/cs/advprog/yomu/dto/QuizSubmitResponse.java
+++ src/main/java/id/ac/ui/cs/advprog/yomu/dto/QuizSubmitResponse.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.yomu.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.util.Map;
 
 @Getter
 @Setter
+@Builder
 public class QuizSubmitResponse {
   private int score;
   private double accuracy;
